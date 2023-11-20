051930d5e10b3b31e32acbf8fdc31ff9e15a6cba
jihoo2002 <yolohoo@naver.com>
2023-11-20 16:13:05 +0900

    jpql 공부

--- jpa/src/main/java/com/study/jpa/chap02_querymethod/repository/StudentRepository.java
+++ jpa/src/main/java/com/study/jpa/chap02_querymethod/repository/StudentRepository.java
@@ -2,6 +2,7 @@ package com.study.jpa.chap02_querymethod.repository;
 
 import com.study.jpa.chap02_querymethod.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -22,5 +23,28 @@ public interface StudentRepository extends JpaRepository<Student, String> {
     Student findNameWithSQL(@Param("nm") String name);
 
 
+    //JPQL
+    //SELECT 별칭 FROM 엔터티 클래스 명 AS 별칭
+    //where 별칭.필드명 =?
+    //SELECT st FROM Student AS st
+    //WHERE st.name = ?
 
+
+    //native-sql
+    //SELECT 컬럼명 FROM 테이블명
+    //WHERE 컬럼 =?
+
+    //도시 이름으로 학생 조회
+    //@Query(value = "SELECT * FROM tbl_student WHERE city = ?1", nativeQuery = true) //첫번째 매개값
+
+    @Query("SELECT s FROM Student s WHERE s.city = ?1") //매개변수 첫번째 | 왜 엔터티로 조회하지?>???
+    List<Student> getByCityWithJPQL(String city);
+
+    @Query("SELECT s FROM Student s WHERE s.name LIKE %:nm%")
+    List<Student> searchByNameWithJPQL(@Param("nm") String name);
+
+    //JPQL로 수정 삭제 쿼리 쓰기
+    @Modifying //조회가 아닐 경우 무조건 붙여야 함
+    @Query("DELETE FROM Student s WHERE s.name Like %:nm%")
+    void  deleteByNameWithJPQL(@Param("nm") String name);
 }
